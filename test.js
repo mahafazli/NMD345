3007702631997c15f64be6968a08f718bd593093
mahafazli <maha.fazli@maine.edu>
2023-12-03 22:28:10 -0500

    added js

--- test.js
+++ test.js
@@ -1,13 +1,42 @@
 const loadAbout = () => {
-    fetch("about.html")
-      .then(response => response.text())
-      .then(
-        text => {
-        document.querySelector("#about").innerHTML = text;
-      })
-      .catch(
-        error => {
-        document.querySelector("#about").innerHTML = "Coming soon!";
-      });
-  };
+  fetch("about.html")
+    .then(response => response.text())
+    .then(
+      text => {
+      document.querySelector("#about").innerHTML = text;
+    })
+    .catch(
+      error => {
+      document.querySelector("#about").innerHTML = "Coming soon!";
+    });
+};
+
+
+let currentSlideOffset = 0 ;
+
+const lastSlideOffset = 6 ;
+
+const slide = direction => {
+  if (direction === "forward") {
+   
+    if ( currentSlideOffset === lastSlideOffset ) {
+     
+      return ;
+    } else {
+      
+      currentSlideOffset ++ ;
+    }
+  } else if ( direction === "back" ) {
   
+    if ( currentSlideOffset === 0 ) {
+      
+      return ;
+    } else {
+      
+      currentSlideOffset -- ;
+    }
+  } ;
+  
+  document.querySelector("#frieze").style.transform = `translateX( ${ currentSlideOffset * -300 }px )` ;
+} ;
+
